d83a0dd7e266a14ddac558fe5cb2d9d7f449d9da
Theray Tharow <tharowt@tharow.net>
2025-01-21 12:42:23 -0500

    Fix BoardConfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,2 +1,13 @@
+# x86_64 emulator specific definitions
+TARGET_CPU_ABI := x86_64
+TARGET_ARCH := x86_64
+TARGET_ARCH_VARIANT := x86_64
+TARGET_2ND_ARCH_VARIANT := x86_64
+
+include device/generic/goldfish/board/BoardConfigCommon.mk
+
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 576716800
+
+
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
